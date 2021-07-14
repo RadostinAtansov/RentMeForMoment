7ea965605eb8b3f089c6246f82a3fb3263bd1be3
Radostin Atanasov <radstatan@gmail.com>
2021-07-14 12:55:20 +0300

    Clean project

---
+++ RentForMoment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RentForMoment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
